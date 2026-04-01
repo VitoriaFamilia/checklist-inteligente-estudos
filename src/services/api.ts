25b483707f1371ebd68a784244e06ea19b53497c
VitoriaFamilia <vitoria.qf123@gmail.com>
2026-04-01 12:06:26 -0300

    fix: removendo rota duplicada da baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,6 @@ import axios from 'axios';
 
 export const api = axios.create({
   // Esta é a URL padrão onde o seu banco de dados fictício vai rodar
-  baseURL: 'https://checklist-inteligente-estudos.onrender.com/tarefas',
+  baseURL: 'https://checklist-inteligente-estudos.onrender.com',
 });
+
